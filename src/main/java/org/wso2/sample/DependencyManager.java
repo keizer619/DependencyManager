db56e248f0a91b5ae4ad64e8d83c734c857e7df1
Tharik Kanaka <onestepfromhell@hotmail.com>
2015-02-09 11:41:19 +0530

    code improved

--- src/main/java/org/wso2/sample/DependencyManager.java
+++ src/main/java/org/wso2/sample/DependencyManager.java
@@ -2,23 +2,15 @@ package org.wso2.sample;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.List;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathFactory;
-import org.eclipse.aether.DefaultRepositorySystemSession;
-import org.eclipse.aether.RepositorySystem;
-import org.eclipse.aether.repository.RemoteRepository;
 import org.wso2.sample.library.Dependency;
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.wso2.sample.util.Booter;
-import org.wso2.sample.util.FileSearch;
 
 /**
  * Created by tharik on 1/29/15.
@@ -27,8 +19,8 @@ public class DependencyManager {
 
     public static final String XPATH_ARTIFACT_SOURCE = "/project/artifactId";
     public static final String XPATH_GROUP_ID = "/project/parent/groupId";
-
-    public static final String POM_FILE_NAME = "pom.xml";
+    public static boolean IS_ALL_POMS = false;
+    public static  String POM_FILE_NAME = "pom.xml";
 
 
     private static ArrayList<Dependency> dependencies = new ArrayList<Dependency>();
@@ -41,33 +33,47 @@ public class DependencyManager {
     public static void processDependencies() throws Exception
     {
         String rootPath = "/Users/tharik/Desktop/git/rep";
-        String repoPath = "/Users/tharik/.m2/repository/org/wso2";
-
         ArrayList<File> pomFiles = DependencyManager.loadPOMFiles(rootPath);
         String json;
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 
+        for (int i= 0 ; i< pomFiles.size(); i++){
+            if (DependencyManager.IS_ALL_POMS ||
+                pomFiles.get(i).getPath().split(File.separator)[rootPath.split(File.separator).length + 1]
+                .equals("pom.xml")) {
 
-        //File folder = new File(repoPath);
-        //File[] repos = folder.listFiles();
+               try {
 
+                 Document doc = dBuilder.parse(pomFiles.get(i));
+                 doc.getDocumentElement().normalize();
 
-        dependencies.addAll(GetDirectDependencies.loadDependenciesFromLocal("org.wso2.cep", "wso2cep", "4.0.0-SNAPSHOT", "product-cep"));
-        dependencies.addAll(GetDirectDependencies.loadDependenciesFromLocal("org.wso2.bam", "wso2bam-parent", "3.0.0-SNAPSHOT", "product-bam"));
-        dependencies.addAll(GetDirectDependencies.loadDependenciesFromLocal("org.wso2.mb", "mb-parent", "3.0.0-SNAPSHOT", "product-mb"));
-        dependencies.addAll(GetDirectDependencies.loadDependenciesFromLocal("org.wso2.greg", "governance-parent", "5.0.0-SNAPSHOT", "product-greg"));
-        dependencies.addAll(GetDirectDependencies.loadDependenciesFromLocal("org.wso2.esb", "wso2esb", "4.8.1", "product-esb"));
-        dependencies.addAll(GetDirectDependencies.loadDependenciesFromLocal("org.wso2.bps", "wso2bps-parent", "3.5.0-SNAPSHOT", "product-bps"));
-        dependencies.addAll(GetDirectDependencies.loadDependenciesFromLocal("org.wso2.identity", "identity-server-parent", "5.1.0-SNAPSHOT", "product-is"));
-        dependencies.addAll(GetDirectDependencies.loadDependenciesFromLocal("org.wso2.ss", "ss-parent", "1.5.0-SNAPSHOT", "product-ss"));
+                 String artifactId = getXpathValue(doc, DependencyManager.XPATH_ARTIFACT_SOURCE);
+                 String groupId = getXpathValue(doc, "/project/groupId");
+                 String version = getXpathValue(doc, "/project/version");
+
+                 if (groupId.equals("")){
+                     groupId = getXpathValue(doc, "/project/parent/groupId");
+                 }
+
+                if (version.equals("")){
+                    version = getXpathValue(doc, "/project/parent/version");
+                }
+
+                dependencies.addAll(GetDirectDependencies.loadDependenciesFromLocal(groupId, artifactId, version,
+                            pomFiles.get(i).getPath().split(File.separator)[rootPath.split(File.separator).length]));
+
+               }
+               catch (Exception ex)
+               {
+                 System.out.println(ex.getMessage());
+               }
+
+            }
+        }
 
         for (int i = 0; i < pomFiles.size(); i++) {
             DependencyManager.loadSourceRepositories(pomFiles.get(i), rootPath);
-        }
-
-        for (int i = 0; i < dependencies.size(); i++){
-            System.out.println(i +" - " +dependencies.get(i).getGroupId() +" - "
-                    + dependencies.get(i).getArtifactId() + " - " + dependencies.get(i).getVersion()
-                    + " - "  + dependencies.get(i).getRepositorySource());
         }
 
         for (int i = 0; i < dependencies.size(); i++){
@@ -86,16 +92,10 @@ public class DependencyManager {
         for (int i = 0; i < uniqueDependencies.size(); i++) {
             json += '"' +uniqueDependencies.get(i).getRepositoryDepends() +'"' + "->" + '"'
                     + uniqueDependencies.get(i).getRepositorySource() + '"' + ";";
-
-            // json += '"' +uniqueDependencies.get(i).getRepositoryDepends() +'"' + "->"
-            // + '"' +uniqueDependencies.get(i).getRepositorySource()
-            // + "(" +uniqueDependencies.get(i).getArtifactId()  + ")" + '"' + ";";
         }
 
         json += "}";
-
         System.out.println(json);
-
         System.out.println("Total Dependencies : " + dependencies.size());
         System.out.println("Total unique Dependencies : " + uniqueDependencies.size());
         System.out.println("Pom Files :" +  pomFiles.size());
@@ -117,88 +117,6 @@ public class DependencyManager {
         }
 
         return  pomFiles;
-    }
-
-    public static ArrayList<Dependency> loadPOM(String rootPath) {
-        FileSearch fileSearch = new FileSearch();
-        String pathArray[]=fileSearch.getPath(rootPath);
-        ArrayList<Dependency> snapshots = new ArrayList<Dependency>();
-
-        for(int i =0;i<pathArray.length;i++){
-            try {
-                File file = new File(pathArray[i]);
-                DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-                DocumentBuilder db = dbf.newDocumentBuilder();
-
-                System.out.println(pathArray[i]);
-
-                String groupId=null;
-                String artifactId=null;
-                String version=null;
-
-                Document doc;
-                try {
-                    doc = db.parse(file);
-                    doc.getDocumentElement().normalize();
-                    NodeList nodeLst = doc.getElementsByTagName("project");
-
-
-                    RepositorySystem system = Booter.newRepositorySystem();
-                    List<RemoteRepository> repositories = new ArrayList<RemoteRepository>();
-
-                    repositories.add((new RemoteRepository.Builder("wso2.snapshots", "default",
-                            "http://maven.wso2.org/nexus/content/repositories/snapshots/")).build());
-                    repositories.add((new RemoteRepository.Builder("wso2.releases", "default",
-                            "http://maven.wso2.org/nexus/content/repositories/releases/")).build());
-                    repositories.add((new RemoteRepository.Builder("wso2.wso2maven2", "default",
-                            "http://maven.wso2.org/nexus/content/repositories/wso2maven2/")).build());
-
-                    DefaultRepositorySystemSession session = Booter.newRepositorySystemSession(system);
-
-
-
-                    for (int s = 0; s < nodeLst.getLength(); s++) {
-                        Node fstNode = nodeLst.item(s);
-
-                        if (fstNode.getNodeType() == Node.ELEMENT_NODE) {
-                            Element newElement = (Element) fstNode;
-                            NodeList groupIdElmntLst = newElement.getElementsByTagName("groupId");
-                            Element groupIdElmnt = (Element) groupIdElmntLst.item(0);
-                            NodeList groupIdNodeList = groupIdElmnt.getChildNodes();
-                            groupId = ((Node) groupIdNodeList.item(0)).getNodeValue();
-
-                            XPath xPath =  XPathFactory.newInstance().newXPath();
-                            artifactId = xPath.compile(DependencyManager.XPATH_ARTIFACT_SOURCE).evaluate(doc);
-
-                            NodeList versionElmntLst = newElement.getElementsByTagName("version");
-                            Element versionElmnt = (Element) versionElmntLst.item(0);
-                            NodeList versionNodeList = versionElmnt.getChildNodes();
-                            version = ((Node) versionNodeList.item(0)).getNodeValue();
-
-                             if(artifactId.contains("parent")){
-                                int index = artifactId.indexOf("parent");
-                                artifactId = artifactId.substring(0,index-1);
-                               }
-
-                            snapshots.addAll(GetDirectDependencies.loadDependencies(groupId, artifactId, version,
-                                    system, session, repositories,
-                                    pathArray[i].split(File.separator)[rootPath.split(File.separator).length]));
-
-                        }
-
-                    }
-
-                } catch (Exception e) {
-                    System.out.println("Document parse error");
-                }
-
-
-            } catch (ParserConfigurationException e) {
-                System.out.println("DocumentBuilder Error");
-            }
-        }
-
-        return snapshots;
     }
 
     public static String getXpathValue(Document doc, String expression)
