c1d31268487e98b6d796978ce6cb9345d44072ea
Nishali <nishaliw@yahoo.com>
2015-02-10 13:23:46 +0530

    data storing

--- src/main/java/org/wso2/sample/Constants.java
+++ src/main/java/org/wso2/sample/Constants.java
@@ -1,5 +1,8 @@
 package org.wso2.sample;
 
+/**
+ * Created by tharik on 2/9/15.
+ */
 public class Constants {
 
     public static final String XPATH_ARTIFACT_SOURCE = "/project/artifactId";
@@ -8,11 +11,13 @@ public class Constants {
     public static final String XPATH_PARENT_GROUP_ID = "/project/parent/groupId";
     public static final String XPATH_PARENT_VERSION = "/project/parent/version";
 
-    public static final String ROOT_PATH = "/Users/tharik/Desktop/git/rep";
+    public static final String ROOT_PATH = "/home/nishali/Documents/snap/git";
     public static boolean IS_ALL_POMS = false;
     public static  String POM_FILE_NAME = "pom.xml";
 
-    public static final String M2_PATH = "/Users/tharik/.m2/repository";
+    public static final String M2_PATH = "/home/nishali/.m2/repository";
     public static final String DEFAULT_SOURCE_NAME = "Other";
     public static final String DEPENDENCY_SEPERATOR = ":";
+    
+    public static final String READ_FILE_PATH="/home/nishali/Documents/Git/DependencyMnager/DependencyManager/out.txt";
 }
